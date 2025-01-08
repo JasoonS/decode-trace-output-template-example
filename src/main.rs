03ce5ff84800b4ee8c098755b9283c885167c230
Jason <jason@envio.dev>
2025-01-08 11:06:01 +0200

    Init

---
+++ src/main.rs
@@ -0,0 +1,90 @@
+use alloy_dyn_abi::DynSolValue;
+use alloy_json_abi::Function;
+use anyhow::{Context, Result};
+use hypersync_client::{
+    net_types::Query, simple_types::Trace, ArrowResponseData, CallDecoder, Client, ClientConfig,
+    FromArrow, StreamConfig,
+};
+use std::sync::Arc;
+
+const GET_USER_ACCOUNT_DATA_SIGNATURE: &str = "function getUserAccountData(address user) external view returns (uint256 totalCollateralBase, uint256 totalDebtBase, uint256 availableBorrowsBase, uint256 currentLiquidationThreshold, uint256 ltv, uint256 healthFactor)";
+
+const AAVE_V3_POOL_ADDRESS: &str = "0x87870Bca3F3fD6335C3F4ce8392D69350B4fA4E2";
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    env_logger::init();
+
+    let client = Arc::new(
+        Client::new(ClientConfig {
+            max_num_retries: Some(10),
+            ..ClientConfig::default()
+        })
+        .unwrap(),
+    );
+
+    let signature = Function::parse(GET_USER_ACCOUNT_DATA_SIGNATURE.as_ref())
+        .context("parse function signature")?
+        .selector();
+
+    let query: Query = serde_json::from_value(serde_json::json!({
+        "from_block": 16291127, // Aave V3 deployment block
+        "traces": [{
+            "sighash": [format!("{:}", signature)],
+            "to": [AAVE_V3_POOL_ADDRESS],
+        }],
+        "field_selection": {
+            "trace": ["input", "output", "block_number"],
+            // "block": ["number", "timestamp"],
+        }
+    }))
+    .unwrap();
+
+    let decoder = CallDecoder::from_signatures(&[GET_USER_ACCOUNT_DATA_SIGNATURE]).unwrap();
+
+    let config = StreamConfig {
+        ..Default::default()
+    };
+
+    let mut rx = client.clone().stream_arrow(query, config).await?;
+
+    fn convert_traces(arrow_response_data: ArrowResponseData) -> Vec<Trace> {
+        arrow_response_data
+            .traces
+            .iter()
+            .flat_map(Trace::from_arrow)
+            .collect()
+    }
+
+    while let Some(result) = rx.recv().await {
+        match result {
+            Ok(response) => {
+                println!("Received response");
+                let traces = convert_traces(response.data);
+                for trace in traces {
+                    if let (Some(input), Some(output), Some(block_number)) =
+                        (trace.input, trace.output, trace.block_number)
+                    {
+                        if let Some(args) = decoder
+                            .decode_input(&input)
+                            .context("Failed to decode input")?
+                        {
+                            if let Some(&DynSolValue::Address(user_address)) = args.get(0) {
+                                println!("Block: {}", block_number);
+                                println!("User: {:#x}", user_address);
+                                println!("Raw output: 0x{}", hex::encode(&output));
+                                // TODO: Decode the output
+                                println!("----------------------------------------");
+                            }
+                        }
+                    }
+                }
+            }
+            Err(e) => {
+                eprintln!("Error: {:?}", e);
+            }
+        }
+    }
+
+    Ok(())
+}
